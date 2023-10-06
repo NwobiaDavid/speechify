36d66876e7c6ad433baaff58ecc5eaf019d6bfbc
NwobiaDavid <dnwobia@gmail.com>
2023-10-06 08:00:08 +0100

    designed the nav bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,17 @@
+import { IBM_Plex_Mono } from 'next/font/google';
+const poppins = IBM_Plex_Mono({
+    subsets: ['latin'],
+    weight: '500'
+});
+
 export default function NavBar() {
   return (
-   <a href="/">
-    <div>
-        Nav
+  
+    <div className = "p-3 bg-gray-500 text-white flex items-center justify-center text-2xl" >
+         <a href="/" className={poppins.className} >
+       ✨ Speechify ✨
+        </a>
     </div>
-   </a>
+ 
   )
 }
